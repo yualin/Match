3c229eafe4b9f775265777fba4aa1575faf6680c
Yuan Lin <lin@addteq.com>
2012-01-16 16:16:28 -0500

    Initial commit.

---
+++ match.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use Time::Piece;
+use Data::Dumper;
+
+my $payments = [
+    # PID, amounts, date
+    [1, 2000, '2010-11-25'],
+    [2, 2050, '2012-11-02'],
+    [3, 4000, '2011-12-20'],
+    [4, 2000, '2012-11-03'],
+];
+
+my $invoices = [
+    # IID, amounts, date
+    [1, 2000, '2010-11-24'],
+    [1, 2000, '2012-11-01'],
+    [1, 50, '2012-11-01'],
+    [1, 2000, '2010-11-24'],
+    [1, 2000, '2010-11-24'],
+
+];
+
+# print Dumper($payments);
+# print Dumper(@{$invoices});
+
+sub time_diff {
+    my ($a, $b) = @_;
+
+    my $aa = Time::Piece->strptime($a, "%Y-%m-%d");
+    my $bb = Time::Piece->strptime($b, "%Y-%m-%d");
+
+    my $diff = $aa - $bb;
+    return abs(int($diff->days));
+}
+
+# This function generate the hash which key is the amount, and the value is
+# array of entry [ID, date].
+sub init_structure {
+    my $input = shift;
+    my $a = {};
+    print Dumper($input);
+    for (@{$input}) {
+ 	my ($ID, $amount, $date) = @{$_};
+	print "ID: $ID, amount: $amount, date: $date.\n";
+	if (defined($a->{$amount})) {
+	    # We have seen the same amount before. Add this item to the list.
+	    push(@{$a->{$amount}}, [$ID, $date]);
+	} else {
+	    # This is the first time we see the amount, create a new entry.
+	    $a->{$amount} = [[$ID, $date]];
+	}
+
+    }
+
+    return $a;
+}
+
+# sub bydate {
+    
+# }
+
+# my $c = init_structure($payments);
+# print Dumper($c);
+
+# The matching function
+sub match {
+    my $payment_struc = init_structure($payments);
+    my $invoices_struc = init_structure($invoices);
+
+    # All these 3 return structures are all in the format of 
+    # { Payment ID # 1 => [ Matched invoice ID #1, ID #2, ... ]
+    #   Payment ID # 2 => [ Matched invoice ID #1, ID #2, ... ]
+    # }
+    my $matched = {};
+    my $candidates = {};
+    my $user_confirm = {};
+
+    
+}
